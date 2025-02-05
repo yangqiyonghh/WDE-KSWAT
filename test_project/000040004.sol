bf5e104e7eeb9ff47a3e358b9d3b7ba23348f7e9
xixi <103592163+yangqiyonghh@users.noreply.github.com>
2025-02-05 16:03:57 +0800

    1
    1

---
+++ test_project/000040004.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:38 Subbasin:4 HRU:4 Luse:FRS1 Soil: 11843 Slope: 30-45 2024/5/11 0:00:00 ArcSWAT 2012.10_2.19
+ Soil Name: 11843
+ Soil Hydrologic Group: B
+ Maximum rooting depth(mm) : 1000.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : Clay
+ Depth                [mm]:      300.00     1000.00
+ Bulk Density Moist [g/cc]:        1.27        1.37
+ Ave. AW Incl. Rock Frag  :        0.17        0.12
+ Ksat. (est.)      [mm/hr]:        1.82        1.00
+ Organic Carbon [weight %]:        1.08        0.45
+ Clay           [weight %]:       43.00       46.00
+ Silt           [weight %]:       33.00       31.00
+ Sand           [weight %]:       24.00       23.00
+ Rock Fragments   [vol. %]:        7.00       10.00
+ Soil Albedo (Moist)      :        0.01        0.01
+ Erosion K                :        0.28        0.30
+ Salinity (EC, Form 5)    :        0.10        0.10
+ Soil pH                  :        4.80        4.80
+ Soil CACO3               :        0.00        0.00
+                              
---
+++ test_project/000800001.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:510 Subbasin:80 HRU:1 Luse:FRS5 Soil: 11367 Slope: 0-15 2024/5/11 0:00:00 ArcSWAT 2012.10_2.19
+ Soil Name: 11367
+ Soil Hydrologic Group: C
+ Maximum rooting depth(mm) :  300.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : Loam
+ Depth                [mm]:      300.00
+ Bulk Density Moist [g/cc]:        1.80
+ Ave. AW Incl. Rock Frag  :        0.21
+ Ksat. (est.)      [mm/hr]:       27.34
+ Organic Carbon [weight %]:        2.13
+ Clay           [weight %]:       19.00
+ Silt           [weight %]:       44.00
+ Sand           [weight %]:       37.00
+ Rock Fragments   [vol. %]:       16.00
+ Soil Albedo (Moist)      :        0.01
+ Erosion K                :        0.26
+ Salinity (EC, Form 5)    :        0.10
+ Soil pH                  :        7.50
+ Soil CACO3               :        6.00
+                              
